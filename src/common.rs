b580d911cf78e38e65f107ac5bef22ec944723b7
wangshaowei12388 <1208537865@qq.com>
2025-06-05 20:49:52 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://192.168.3.91:21114".to_owned()
 }
 
 #[inline]
